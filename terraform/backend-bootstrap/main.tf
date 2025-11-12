243eb650dfc20d639ff56774b09bc724c8b32fdb
group2cet11 <group2_cet11@protonmail.com>
2025-11-12 19:01:35 +0800

    Update main.tf

--- terraform/backend-bootstrap/main.tf
+++ terraform/backend-bootstrap/main.tf
@@ -70,12 +70,17 @@ resource "aws_s3_bucket_public_access_block" "tf" {
 
 # --- DynamoDB table for state locking ---
 resource "aws_dynamodb_table" "locks" {
-  name         = var.lock_table_name
+  name         = var.lock_table_name         # "terraform-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
   attribute {
     name = "LockID"
     type = "S"
+  }
+
+  # ⇩ Add this block
+  lifecycle {
+    prevent_destroy = true
   }
 }
