9c04a0f34ddd799f270799a8d73803d579f82c38
mjmak0001 <mjmak001@gmail.com>
2025-12-14 20:27:28 +0800

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
-output "public_ip" {
-  value = module.compute.public_ip
-}
+#############################################
+# terraform/outputs.tf (delete or comment out the duplicate)
+#############################################
+# Remove or comment out this block to avoid duplicate output
+# output "public_ip" {
+#   value = module.compute.public_ip
+# }
