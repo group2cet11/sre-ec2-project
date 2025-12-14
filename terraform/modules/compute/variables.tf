eb5531d0bd375cea4ecf42f446075d6f4acc7f19
mjmak0001 <mjmak001@gmail.com>
2025-12-14 19:16:06 +0800

    Update variables.tf

--- terraform/modules/compute/variables.tf
+++ terraform/modules/compute/variables.tf
@@ -1,8 +1,48 @@
-variable "subnet_id"        { type = string }
-variable "vpc_id"           { type = string }
-variable "instance_type"    { type = string }
-variable "ami_id"           { type = string }
-variable "environment"      { type = string }
-variable "key_name"         { type = string }
-variable "userdata_revision" { type = number }
-variable "name_prefix"      { type = string}
+#############################################
+# terraform/modules/compute/variables.tf
+#############################################
+variable "subnet_id" {
+  type        = string
+  description = "Subnet ID for the EC2 instance"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "EC2 instance type"
+}
+
+variable "ami_id" {
+  type        = string
+  description = "AMI ID for the EC2 instance"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name (dev/uat/prod)"
+}
+
+variable "key_name" {
+  type        = string
+  description = "EC2 key pair name for SSH access"
+}
+
+variable "userdata_revision" {
+  type        = number
+  description = "Revision number to force user_data change"
+  default     = 1
+}
+
+variable "name_prefix" {
+  type        = string
+  description = "Prefix for resource names"
+}
+
+variable "prometheus_ecs_sg_id" {
+  type        = string
+  description = "Security group ID of the Prometheus Fargate tasks (to allow Node Exporter access on port 9100)"
+}
