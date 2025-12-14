2edcb0ed234caf88992062bdbefaa8b6cf79176d
mjmak0001 <mjmak001@gmail.com>
2025-12-14 21:21:05 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 #############################################
-# terraform/main.tf (clean - no duplicates)
+# terraform/main.tf (updated - fixed log group duplicate error)
 #############################################
 terraform {
   required_version = ">= 1.5.0"
@@ -13,16 +13,58 @@ terraform {
   backend "s3" {}
 }
 
+variable "environment" {
+  type = string
+  description = "Environment name (dev/uat/prod)"
+}
+
+variable "region" {
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami_id" {
+  type        = string
+  default     = "ami-0c101f26f147fa7fd"
+}
+
+variable "key_name" {
+  type        = string
+  description = "SSH key pair name"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "Subnet ID for the EC2 instance"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID"
+}
+
+variable "userdata_revision" {
+  type        = number
+  default     = 1
+}
+
+variable "project" {
+  type        = string
+  default     = "sre"
+}
+
 locals {
   prefix = "${var.project}-${var.environment}"
 }
 
-resource "aws_cloudwatch_log_group" "ec2" {
-  name              = "/sre/${var.environment}/ec2"
-  retention_in_days = 14
-  tags = {
-    Environment = var.environment
-  }
+# Import existing log group instead of creating (avoids "already exists" error)
+data "aws_cloudwatch_log_group" "ec2" {
+  name = "/sre/${var.environment}/ec2"
 }
 
 module "compute" {
@@ -38,7 +80,7 @@ module "compute" {
 }
 
 output "log_group_name" {
-  value = aws_cloudwatch_log_group.ec2.name
+  value = data.aws_cloudwatch_log_group.ec2.name
 }
 
 output "public_ip" {
